34a1e9b93d1fc571d025421ee2dcc16c48eebfa9
Brion Vibber <brion@pobox.com>
2011-12-17 13:10:37 -0800

    stub: draws some fake data to canvas

---
+++ app.js
@@ -0,0 +1,34 @@
+$(function() {
+
+	var $canvas = $('#brot'),
+		canvas = $canvas[0],
+		ctx = canvas.getContext('2d'),
+		width = parseInt($canvas.attr('width')),
+		height = parseInt($canvas.attr('height'));
+
+	function runBrot(threads) {
+		$('#pixel-count').text(width * height);
+		
+		var startTime = Date.now();
+
+		for (var y = 0; y < height; y++) {
+			var row = ctx.createImageData(width, 1),
+				data = row.data,
+				i = 0;
+			
+			for (var x = 0; x < width; x++) {
+				data[i++] = x % 256;
+				data[i++] = y % 256;
+				data[i++] = x % 256;
+				data[i++] = 255;
+			}
+			ctx.putImageData(row, 0, y);
+		}
+		
+		var delta = Date.now() - startTime;
+		$('#pps').text((width * height) / (delta / 1000));
+	}
+	
+	runBrot(1);
+
+});
